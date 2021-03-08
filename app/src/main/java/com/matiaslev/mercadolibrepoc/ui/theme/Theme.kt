bac8f517af4bd158ac1b206566f7ff8b78daee9f
matias lev <matias.lev@uala.com.ar>
2021-03-08 09:38:21 -0300

    dark mode asi light mode for now

--- app/src/main/java/com/matiaslev/mercadolibrepoc/ui/theme/Theme.kt
+++ app/src/main/java/com/matiaslev/mercadolibrepoc/ui/theme/Theme.kt
@@ -33,7 +33,7 @@ fun MercadoLibrePocTheme(
     content: @Composable() () -> Unit
 ) {
     val colors = if (darkTheme) {
-        DarkColorPalette
+        LightColorPalette
     } else {
         LightColorPalette
     }
